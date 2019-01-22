0f2f9bfc4e2b891c6bb5d97ab7214ecf7bdb18ae
Marco Antonio Parente Nunes <marcoparente@MacBook-Pro-de-Marco.local>
2019-01-22 11:30:56 -0300

    Adicionado CRUD users

--- index.js
+++ index.js
@@ -8,8 +8,10 @@
  var https = require('https');
  var url = require('url');
  var StringDecoder = require('string_decoder').StringDecoder;
- var config = require('./config');
+ var config = require('./lib/config');
  var fs = require('fs');
+ var handlers = require('./lib/handlers');
+ var helpers = require('./lib/helpers');
  
  
 // Instantiating the HTTP server
@@ -72,7 +74,7 @@ var unifiedServer = function(req,res){
          'queryStringObject' : queryStringObject,
          'method' : method,
          'headers' : headers,
-         'payload' : buffer
+         'payload' : helpers.parseJsonToObject(buffer)
       };
 
       // Route the request to the handler specified in the router
@@ -98,20 +100,9 @@ var unifiedServer = function(req,res){
     });
 };
 
- // Define handlers
- var handlers = {};
-
-// Not Found handler
-handlers.notFound = function(data,callback){
-   callback(404);
-};
-
-// Ping handler
-handlers.ping = function(data,callback){
-   callback(200);
-};
 
  // Define a request router
  var router = {
-   'ping' :  handlers.ping
+   'ping' :  handlers.ping,
+   'users' : handlers.users
  };
