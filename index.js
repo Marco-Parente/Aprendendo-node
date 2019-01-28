89d090eaa755fbedfa2e9ae0758e42709607acbb
Marco Antonio Parente Nunes <marcoparente@MacBook-Pro-de-Marco.local>
2019-01-28 08:03:01 -0300

    Added 'workers', sec 28

--- index.js
+++ index.js
@@ -4,107 +4,23 @@
  */ 
 
 // Dependencies
- var http = require('http');
- var https = require('https');
- var url = require('url');
- var StringDecoder = require('string_decoder').StringDecoder;
- var config = require('./lib/config');
- var fs = require('fs');
- var handlers = require('./lib/handlers');
- var helpers = require('./lib/helpers');
- 
- 
-// Instantiating the HTTP server
- var httpServer = http.createServer(function(req,res){
-   unifiedServer(req,res);
-   });
+var server = require('./lib/server');
+var workers = require('./lib/workers');
 
-// Start the server
- httpServer.listen(config.httpPort,function(){
-    console.log('The server is listening on port '+config.httpPort);
- });
+// Declare the app
+var app = {};
 
- // Instantiate the HTTPS server
- httpsServerOptions = {
-   'key' : fs.readFileSync('./https/key.epm'),
-   'cert' : fs.readFileSync('./https/cert.pem')
- };
- var httpsServer = https.createServer(httpsServerOptions,function(req,res){
-   unifiedServer(req,res);
-   });
+// Init function
+app.init = function(){
+   // Start the server
+   server.init();
 
- // Start the HTTPS server
- httpsServer.listen(config.httpsPort,function(){
-   console.log('The server is listening on port '+config.httpsPort);
-});
-
- // All the server logic for both http and https server
-var unifiedServer = function(req,res){
-   // Get the URL and parse it
-    parsedUrl = url.parse(req.url,true);
-
-    // Get the path of the URL
-    var path = parsedUrl.pathname;
-    var trimmedPath = path.replace(/^\/+|\/+$/g,'');
-
-    // Get the query string as an object
-    var queryStringObject = parsedUrl.query;
-
-    // Get the HTTP method
-    var method = req.method.toLowerCase();
-
-    // Get the headers as an object
-    var headers = req.headers;
-
-    // Get the payload, if any
-    var decoder = new StringDecoder('utf-8');
-    var buffer = '';
-    req.on('data',function(data){
-        buffer += decoder.write(data);
-    });
-    req.on('end', function(){
-       buffer += decoder.end();
-
-       // Choose the handler this request sould go to. If one is not found, choose the notFound handler
-       var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-       // Construct the data object to send to the handler
-      var data = {
-         'trimmedPath' : trimmedPath,
-         'queryStringObject' : queryStringObject,
-         'method' : method,
-         'headers' : headers,
-         'payload' : helpers.parseJsonToObject(buffer)
-      };
-
-      // Route the request to the handler specified in the router
-      chosenHandler(data, function(statusCode, payload){
-         // Use the status code called back by the handler, or default to 200
-         statusCode : typeof(statusCode) == 'number' ? statusCode : 200;
-
-         // Use the payload called back by the handler, or to an empty object
-         payload : typeof(payload) == 'object' ? payload : {};
-
-         // Convert the payload to a string
-         var payloadString = JSON.stringify(payload);
-
-         // Return the response
-         res.setHeader('Content-Type','application/json');
-         res.writeHead(statusCode);
-         res.end(payloadString);
-
-         // Log the request path 
-         console.log('Returning this response: ', statusCode, payloadString);
-
-      });
-    });
+   // Start the workers
+   workers.init();
 };
 
+// Execute
+app.init();
 
- // Define a request router
- var router = {
-   'ping' :  handlers.ping,
-   'users' : handlers.users,
-   'tokens' : handlers.tokens,
-   'checks' : handlers.checks
- };
+// Export the app
+module.exports = app;
